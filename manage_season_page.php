fe0fd013044119e4676fa899f83bf30a4f1e04b4
Justin Lee <leejustin464@gmail.com>
2026-05-04 15:01:03 -0700

    flesh skeleton files

--- manage_season_page.php
+++ manage_season_page.php
@@ -3,7 +3,7 @@
   require_once('html_components.php');
   require_once('helpers.php');
 
-  do_html_header('Manage Players');
+  do_html_header('Manage Seasons');
   check_valid_user();
   display_user_nav();
   do_html_footer();
